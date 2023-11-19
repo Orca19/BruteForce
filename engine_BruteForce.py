37e83f6d7d5a0a9221b2154e30273051f77cc5ba
Orca19 <yarinm1910@gmail.com>
2023-11-19 15:43:55 +0200

    Update engine_BruteForce.py

--- engine_BruteForce.py
+++ engine_BruteForce.py
@@ -8,7 +8,7 @@ data = 'amogus'
 soc.send(data.encode('uf-8'))
 time.sleep(0.1)
 answer  = soc.recv(1024).decode('utf-8')
-soc.send(answer.encode('uf-8'))
+soc.send(answer.encode('utf-8'))
 time.sleep(0.1)
 answer  = soc.recv(1024).decode('utf-8')
 inputs = answer.split(',')
@@ -16,6 +16,8 @@ start = inputs[0][6:]
 stop = inputs[1][5:]
 MD5 = input[2][4:]
 start_list = [chr for chr in start]
-BruteForceV2.BruteForce(start, stop,start_list, MD5)
+password = BruteForceV2.BruteForce(start, stop,start_list, MD5)
+soc.send(password.encode('utf-8'))
+
 
 
