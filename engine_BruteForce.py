c547508eebb96e18b225b1ca03a91d1d1b1b24e6
Orca19 <yarinm1910@gmail.com>
2023-11-19 16:20:46 +0200

    update

--- engine_BruteForce.py
+++ engine_BruteForce.py
@@ -10,14 +10,25 @@ time.sleep(0.1)
 answer  = soc.recv(1024).decode('utf-8')
 soc.send(answer.encode('utf-8'))
 time.sleep(0.1)
-answer  = soc.recv(1024).decode('utf-8')
-inputs = answer.split(',')
-start = inputs[0][6:]
-stop = inputs[1][5:]
-MD5 = input[2][4:]
-start_list = [chr for chr in start]
-password = BruteForceV2.BruteForce(start, stop,start_list, MD5)
-soc.send(password.encode('utf-8'))
+while True:
+    answer  = soc.recv(1024).decode('utf-8')
+    time.sleep(0.1)
+    soc.send('ok'.encode('utf-8'))
+    inputs = answer.split(',')
+    start = inputs[0][6:]
+    stop = inputs[1][5:]
+    MD5 = input[2][4:]
+    start_list = [chr for chr in start]
+    password = BruteForceV2.BruteForce(start, stop,start_list, MD5)
+    if password!='password was not found':
+        answer = 'success '
+    else:
+        answer = 'failed '
+    answer+=password
+    soc.send(answer.encode('utf-8'))
+    answer  = soc.recv(1024).decode('utf-8')
+    soc.send('next'.encode('utf-8'))
+
 
 
 
