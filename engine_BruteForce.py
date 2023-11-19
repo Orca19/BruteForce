c6c51357674ee3ffb6a52edee30a7ae11479dcd7
Orca19 <116515626+Orca19@users.noreply.github.com>
2023-11-19 15:39:01 +0200

    Add files via upload

---
+++ engine_BruteForce.py
@@ -0,0 +1,21 @@
+from socket import *
+import time
+import BruteForceV2
+soc = socket()
+addr = ('0,0,0,0', 2222)
+soc.connect(addr)
+data = 'amogus'
+soc.send(data.encode('uf-8'))
+time.sleep(0.1)
+answer  = soc.recv(1024).decode('utf-8')
+soc.send(answer.encode('uf-8'))
+time.sleep(0.1)
+answer  = soc.recv(1024).decode('utf-8')
+inputs = answer.split(',')
+start = inputs[0][6:]
+stop = inputs[1][5:]
+MD5 = input[2][4:]
+start_list = [chr for chr in start]
+BruteForceV2.BruteForce(start, stop,start_list, MD5)
+
+
