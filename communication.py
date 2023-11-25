6e75cd16e4fe05856a743f7ef950daf86396bd0d
Orca19 <yarinm1910@gmail.com>
2023-11-25 18:03:03 +0200

    up

--- communication.py
+++ communication.py
@@ -11,34 +11,41 @@ def blocking(soc, end):
         
 def com(soc, end):
     while not end:
-        answer  = soc.recv(1024).decode('utf-8')
-        time.sleep(0.1)
-        soc.send('ok'.encode('utf-8'))
-        inputs = answer.split(',')
-        start = inputs[0][6:]
-        stop = inputs[1][5:]
-        MD5 = input[2][4:]
-        start_list = [chr for chr in start]
-        password = BruteForceV2.BruteForce(start, stop,start_list, MD5)
-        if password!='password was not found':
-            answer = 'success '
-        else:
-            answer = 'failed '
-        answer+=password
-        soc.send(answer.encode('utf-8'))
-        answer  = soc.recv(1024).decode('utf-8')
-        if answer!='ok':
-            break
-        soc.send('next'.encode('utf-8'))
+        answer  = soc.recv(1024).decode()
+        if answer[:6]=='start:':
+            time.sleep(0.1)
+            soc.send('ok'.encode('utf-8'))
+            print("got data")
+            inputs = answer.split(',')
+            start = inputs[0][6:]
+            stop = inputs[1][5:]
+            MD5 = inputs[2][4:]
+            start_list = [chr for chr in start]
+            password = BruteForceV2.BruteForce(start, stop,start_list, MD5)
+            if password!='password was not found':
+                answer = 'success '
+            else:
+                answer = 'failed '
+            answer+=password
+            print(password)
+            soc.send(answer.encode('utf-8'))
+            print("password was sent")
+            time.sleep(0.1)
+            answer  = soc.recv(1024).decode('utf-8')
+            print(answer)
+            if answer!='ok':
+                break
+            soc.send('next'.encode('utf-8'))
     soc.close()
 def main(soc, end):
     threads = []
-    t1 = threading.Thread(target = blocking, args = (soc,end))
-    t2 = threading.Thread(target = com, args = (soc,end))
+    t1 = threading.Thread(target = com, args = (soc,end))
+    t2 = threading.Thread(target = blocking, args = (soc,end))
     threads.append(t1)
     threads.append(t2)
     for t in threads:
         t.start()
+    print("started threads")
 
 if __name__ =='__main__':
     main()
--- engine_BruteForce.py
+++ engine_BruteForce.py
@@ -6,15 +6,18 @@ global end
 end = False
 
 soc = socket()
-addr = ('0,0,0,0', 2222)
+addr = ('192.168.1.102', 52000)
 soc.connect(addr)
+print("connected")
 data = 'amogus'
-soc.send(data.encode('uf-8'))
+soc.send(data.encode('utf-8'))
+print("sent word")
 time.sleep(0.1)
 answer  = soc.recv(1024).decode('utf-8')
+print("recieved word")
 soc.send(answer.encode('utf-8'))
 time.sleep(0.1)
-
+print("entering communication")
 communication.main(soc,end)
 
 
